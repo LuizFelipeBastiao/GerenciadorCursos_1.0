89b050e2a6f723e80317f39cfc93e02a9d1324d3
Luiz Felipe Bastião <Luiz Felipe Bastio@FELIPESDESKTOP>
2024-10-21 08:39:35 -0300

    Feito mais algumas alterações nas páginas

--- GerenciadorCursos/Matricula.xaml.cs
+++ GerenciadorCursos/Matricula.xaml.cs
@@ -1,9 +1,34 @@
-namespace GerenciadorCursos;
-
-public partial class Matricula : ContentPage
+namespace GerenciadorCursos
 {
-	public Matricula()
-	{
-		InitializeComponent();
-	}
+    public partial class Matricula : ContentPage
+    {
+        public Matricula()
+        {
+            InitializeComponent();
+        }
+
+        private void AoMarcarCheckBoxMF(object sender, CheckedChangedEventArgs e)
+        {
+            if (sender == MasculinoCheckBox && e.Value)
+            {
+                FemininoCheckBox.IsChecked = !MasculinoCheckBox.IsChecked;
+            }
+            else if (sender == FemininoCheckBox && e.Value)
+            {
+                MasculinoCheckBox.IsChecked = !FemininoCheckBox.IsChecked;
+            }
+        }
+
+        private void AoMarcarCheckBoxSC(object sender, CheckedChangedEventArgs e)
+        {
+            if (sender == SolteiroCheckBox && e.Value)
+            {
+                CasadoCheckBox.IsChecked = !SolteiroCheckBox.IsChecked;
+            }
+            else if (sender == CasadoCheckBox && e.Value)
+            {
+                SolteiroCheckBox.IsChecked = !CasadoCheckBox.IsChecked;
+            }
+        }
+    }
 }
