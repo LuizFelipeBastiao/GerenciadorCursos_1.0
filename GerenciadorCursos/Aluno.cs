3ba0ae0344fba526edf8e33f4197ae51eff35e11
Luiz <lbastiao@alunos.utfpr.edu.br>
2024-10-24 12:06:29 -0300

    adicionado variaveis ao formulário

--- GerenciadorCursos/Aluno.cs
+++ GerenciadorCursos/Aluno.cs
@@ -1,22 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GerenciadorCursos;
+﻿namespace GerenciadorCursos;
 public class Aluno
 {
-    public string nome { get; set; }
-    public string email { get; set; }
+    public string Nome { get; set; }
+    public string Email { get; set; }
 
-    public string dataNasc { get; set; }
+    public string DataNascimento { get; set; }
 
-    public string cpf { get; set; }
+    public string CPF { get; set; }
 
-    public string rg { get; set; }
+    public string RG { get; set; }
 
-    public string endereco { get; set; }
+    public string Endereco { get; set; }
 
-    public string telefone { get; set; }
+    public string Telefone { get; set; }
+
+    public override string ToString()
+    {
+        return $"[{Nome},{Email}, {DataNascimento}]";
+    }
 }
--- GerenciadorCursos/Matricula.xaml.cs
+++ GerenciadorCursos/Matricula.xaml.cs
@@ -1,34 +1,43 @@
-namespace GerenciadorCursos
+namespace GerenciadorCursos;
+
+public partial class Matricula : ContentPage
 {
-    public partial class Matricula : ContentPage
+    public Matricula()
     {
-        public Matricula()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+    }
 
-        private void AoMarcarCheckBoxMF(object sender, CheckedChangedEventArgs e)
-        {
-            if (sender == MasculinoCheckBox && e.Value)
-            {
-                FemininoCheckBox.IsChecked = !MasculinoCheckBox.IsChecked;
-            }
-            else if (sender == FemininoCheckBox && e.Value)
-            {
-                MasculinoCheckBox.IsChecked = !FemininoCheckBox.IsChecked;
-            }
-        }
+    private void AoClicarConfirmar(object sender, EventArgs e)
+    {
+        Aluno aluno = new Aluno();
 
-        private void AoMarcarCheckBoxSC(object sender, CheckedChangedEventArgs e)
+        aluno.Nome = NomeEntry.Text;
+        aluno.Email = EmailEntry.Text;
+
+        Informacoes.Text = aluno.ToString();
+    }
+
+private void AoMarcarCheckBoxMF(object sender, CheckedChangedEventArgs e)
+    {
+        if (sender == MasculinoCheckBox && e.Value)
         {
-            if (sender == SolteiroCheckBox && e.Value)
-            {
-                CasadoCheckBox.IsChecked = !SolteiroCheckBox.IsChecked;
-            }
-            else if (sender == CasadoCheckBox && e.Value)
-            {
-                SolteiroCheckBox.IsChecked = !CasadoCheckBox.IsChecked;
-            }
+            FemininoCheckBox.IsChecked = !MasculinoCheckBox.IsChecked;
+        }
+        else if (sender == FemininoCheckBox && e.Value)
+        {
+            MasculinoCheckBox.IsChecked = !FemininoCheckBox.IsChecked;
+        }
+    }
+
+    private void AoMarcarCheckBoxSC(object sender, CheckedChangedEventArgs e)
+    {
+        if (sender == SolteiroCheckBox && e.Value)
+        {
+            CasadoCheckBox.IsChecked = !SolteiroCheckBox.IsChecked;
+        }
+        else if (sender == CasadoCheckBox && e.Value)
+        {
+            SolteiroCheckBox.IsChecked = !CasadoCheckBox.IsChecked;
         }
     }
 }
