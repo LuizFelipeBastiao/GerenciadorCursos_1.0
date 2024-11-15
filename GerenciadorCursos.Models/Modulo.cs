020f4463d83442b43dd993d7a3907c391aef6d69
Yasmin <yasminyamamoto48@gmail.com>
2024-11-15 14:19:10 -0300

    criei a clasese modulo

---
+++ GerenciadorCursos.Models/Modulo.cs
@@ -0,0 +1,18 @@
+﻿
+using Models;
+
+namespace GerenciadorCursos.Models
+{
+    internal class Modulo
+    {
+        int Id { get; set; }
+        string Titulo { get; set; }
+        Curso Curso { get; set; }
+        ICollection<Licao> licoes { get; set; }
+
+        public Modulo()
+        {
+            licoes  = new List<Licao>();
+        }
+    }
+}
